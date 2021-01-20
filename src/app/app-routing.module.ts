ac9f7c6da5a149a7b9f85d859bff30f0e61fe933
miketascon <maicol_tascon@hotmail.com>
2021-01-20 16:42:32 -0500

    inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'pages/home', pathMatch: 'full'},
+  {path: 'pages', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule)},
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
