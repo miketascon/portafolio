746a83e635bacc6ef4a0ebd3e717456196ab5b6c
Andres Tascon <miketascon@misena.edu.co>
2021-01-25 19:52:23 -0500

    terminado queda faltando email y resumen

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $;
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -12,6 +14,31 @@ export class HomeComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+
+    $(window).on('scroll', (): void => {
+      if ($(window).scrollTop()) {
+          $('.section').addClass('animate__animated animate__backInLeft');
+          $('.image-perfil').addClass('animate__animated animate__backInLeft');
+          $('.parrafo').addClass('animate__animated animate__backInLeft');
+          $('.experiencia').addClass('animate__animated animate__backInLeft');
+          $('.efect-rigth').addClass('animate__animated animate__backInRight');
+          $('.efect-left').addClass('animate__animated animate__backInLeft');
+          $('.proyectos').addClass('animate__animated animate__backInLeft');
+          $('.contacto').addClass('animate__animated animate__backInUp');
+      } else {
+        $('nav').removeClass('animate__animated animate__backInLeft');
+        $('.image-perfil').removeClass('animate__animated animate__backInLeft');
+        $('.parrafo').removeClass('animate__animated animate__backInLeft');
+        $('.experiencia').removeClass('animate__animated animate__backInLeft');
+        $('.efect-rigth').removeClass('animate__animated animate__backInRight');
+        $('.efect-left').removeClass('animate__animated animate__backInLeft');
+        $('.proyectos').removeClass('animate__animated animate__backInLeft');
+        $('.contacto').removeClass('animate__animated animate__backInUp');
+      }
+    });
+
+
+
   }
 
 }
